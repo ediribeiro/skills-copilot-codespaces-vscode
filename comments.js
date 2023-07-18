6d5cfa82d7d1c6969bc5ddd43bbb18a584212bdc
ediribeiro <edu.alri@gmail.com>
2023-07-18 14:32:31 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,36 +1,68 @@
 // create web server
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const morgan = require('morgan')
+// run node comments.js
+// go to http://localhost:3000/comments.html
 
-// create web server
-const app = express()
-app.use(morgan('combined'))
-app.use(bodyParser.json())
-app.use(cors())
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-// create a get request
-app.get('/posts/:id/comments', (req, res) => {
-  res.send([
-    {
-      id: 1,
-      comment: 'comment 1'
-    },
-    {
-      id: 2,
-      comment: 'comment 2'
+var COMMENTS_FILE = 'comments.json';
+
+// create server
+http.createServer(function(req, res) {
+    // get url
+    var url_parts = url.parse(req.url);
+    console.log(url_parts);
+
+    // get file name
+    var path = url_parts.pathname;
+    console.log(path);
+
+    // get file extention
+    var ext = path.split('.').pop();
+    console.log(ext);
+
+    // get query string
+    var query = url_parts.query;
+    console.log(query);
+
+    // get file name
+    var filename = path.substring(1);
+    console.log(filename);
+
+    // default page
+    if (path == '/') {
+        filename = 'index.html';
     }
-  ])
-})
 
-// create a post request
-app.post('/posts/:id/comments', (req, res) => {
-  res.send('post request')
-})
+    // get file type
+    var type = 'text/html';
+    switch (ext) {
+        case 'css':
+            type = 'text/css';
+            break;
+        case 'js':
+            type = 'text/javascript';
+            break;
+        default:
+            type = 'text/html';
+            break;
+    }
 
-// listen to the port
-app.listen(4001, () => {
-  console.log('listening on port 4001')
-})
+    // read file
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            // error
+            console.log(err);
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.write('404 Not Found\n');
+        } else {
+            // success
+            res.writeHead(200, { 'Content-Type': type });
+            res.write(data);
+        }
+        res.end();
+    });
+}).listen(3000);
 
+console.log('Server running at http://localhost:3000/');
