ca8853e621e46d0e2f4fcb7993f29ab59f8db922
ediribeiro <edu.alri@gmail.com>
2023-07-18 14:29:02 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,95 +1,36 @@
 // create web server
-const express = require("express");
-const router = express.Router();
-const Post = require("../models/Post");
-const Comment = require("../models/Comment");
-const User = require("../models/User");
-const { Op } = require("sequelize");
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
 
-// create comment
-router.post("/:postId", async (req, res) => {
-  try {
-    const comment = await Comment.create({
-      content: req.body.content,
-      postId: req.params.postId,
-      userId: req.body.userId
-    });
-    res.status(201).json(comment);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
+// create web server
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
 
-// get all comments
-router.get("/", async (req, res) => {
-  try {
-    const comments = await Comment.findAll();
-    res.status(200).json(comments);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-// get all comments by post id
-router.get("/:postId", async (req, res) => {
-  try {
-    const comments = await Comment.findAll({
-      where: { postId: req.params.postId },
-      include: [
-        {
-          model: User,
-          attributes: ["username"]
-        }
-      ]
-    });
-    res.status(200).json(comments);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-// get all comments by user id
-router.get("/user/:userId", async (req, res) => {
-  try {
-    const comments = await Comment.findAll({
-      where: { userId: req.params.userId },
-      include: [
-        {
-          model: Post,
-          attributes: ["title"]
-        }
-      ]
-    });
-    res.status(200).json(comments);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-// update comment
-router.patch("/:commentId", async (req, res) => {
-  try {
-    const comment = await Comment.update(
-      {
-        content: req.body.content
-      },
-      { where: { id: req.params.commentId } }
-    );
-    res.status(200).json(comment);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-// delete comment
-router.delete("/:commentId", async (req, res) => {
-    try {
-        const comment = await Comment.destroy({
-        where: { id: req.params.commentId }
-        });
-        res.status(200).json(comment);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
+// create a get request
+app.get('/posts/:id/comments', (req, res) => {
+  res.send([
+    {
+      id: 1,
+      comment: 'comment 1'
+    },
+    {
+      id: 2,
+      comment: 'comment 2'
     }
-});
+  ])
+})
+
+// create a post request
+app.post('/posts/:id/comments', (req, res) => {
+  res.send('post request')
+})
+
+// listen to the port
+app.listen(4001, () => {
+  console.log('listening on port 4001')
+})
 
