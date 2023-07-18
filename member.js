c1e5f839a0643369aa3a1c84c44f781319a0948b
ediribeiro <edu.alri@gmail.com>
2023-07-18 11:59:52 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberSkills = document.getElementById("memberSkills");
+    memberSkills.style.display = "block";
+    member.style.display = "none";
+    skills.style.display = "none";
+}
